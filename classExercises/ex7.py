e0c94c893255134ce1bbb752df39d7f333f46495
Connie Cheng <connie.cheng52@gmail.com>
2017-09-09 22:14:01 -0400

    added more notes to exercise 7, uploaded exercise 8

--- classExercises/ex7.py
+++ classExercises/ex7.py
@@ -1,6 +1,8 @@
 print("Mary had a little lamb.")
+#puts the argument snow into the reseved {} and prints it
 print("Its fleece was white as {}.".format('snow'))
 print("And everywhere that Mary went.")
+#prints the . 10 times
 print("." * 10) #what'd that do?
 
 end1 = "C"
@@ -18,4 +20,11 @@ end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+#the comma prints a space between the two variables and concatinates it with the next line
 print(end7 + end8 + end9 + end10 + end11 + end12)
+
+#interestingly enough 
+# print(end1 + end2 + end3 + end4 + end5 + end6 + end = ' ') does not work
+# end is a keyword that prints the value at the end of what you wanted to print
+# it cannot be evaluated as an expression which is why it doesn't play nicely with the + sign, it's assuming we want to evaluate what's being done to end
+# in this case we are reassigning the value of the keyword to an empty space instead of the default new line
