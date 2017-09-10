1eb432d9e07ee1d6c5d655ea12a0b8e871cd4f65
Connie Cheng <connie.cheng52@gmail.com>
2017-09-09 20:20:28 -0400

    add assignments and class work

---
+++ homework/ex3.py
@@ -0,0 +1,50 @@
+print("I will now count my chickens:")
+
+# adds 25 to 5.0 (30/6) type casts to float; next line evals multiplication then modulo then subtraction
+print("Hens", 25 + 30 / 6 )
+print("Roosters", 100 - 250 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+# modulo eval first, then dividing, then all the adding and subtracting
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# first line prints a string, evaluated both sides, then check if left is greater than right side, prints out if statement is true or false
+print("Is it true that 3+2<5-7?")
+print(3 + 2 < 5 - 7)
+print("What is 3+2?", 3 + 2)
+print("What is 5-7?", 5 - 7)
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# operator does a comparision and tells you if it's greater than, greater than or equal to or less than or equal to, evals each to true or false
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?",5 <= -2)
+
+''' Study Drill: Convert to floats
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0/ 6.0)
+print("Roosters", 100.0 - 250.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+
+# modulo eval first, then dividing, then all the adding and subtracting
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# first line prints a string, evaluated both sides, then check if left is greater than right side, prints out if statement is true or false
+print("Is it true that 3+2<5-7?")
+print(3.0 + 2.0 < 5.0 - 7.0)
+print("What is 3+2?", 3.0 + 2.0)
+print("What is 5-7?", 5.0 - 7.0)
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# operator does a comparision and tells you if it's greater than, greater than or equal to or less than or equal to, evals each to true or false
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?",5.0 <= -2.0)
+'''
---
+++ homework/hw2.py
@@ -0,0 +1,50 @@
+# Exercise 3 A
+
+#declare function to take in array list and print out average
+def aver(numList):
+    # variables to store numbers
+    avg= 0.0
+    total = 0
+    #iterate through number set and get total
+    for int in numList:
+        total+= int
+    #store the length of numer set
+    given=len(numList)
+    #artimetic to get average
+    avg =total/given
+    #print statment
+    print(f"The average {given} given numbers of is {avg}")
+# i.
+aver([44, 64, 88, 53, 89])
+#ii. 
+aver([39, 45, 55, 90, 95, 96])
+#iii. redefine array/numset
+aver([54, -45, -10, 90])
+#iv. redefine array/numset
+aver([55, 65, 75, 95, 32])
+
+'''
+Alternatively you can hard code the numbers
+i.
+total= 44 + 64 + 88 + 53 + 89
+given = 5
+avg = total/given
+print(f"The average {given} given numbers of is {avg}")
+'''
+
+#Exercise 3b
+def isEven(inputNum):
+    if(inputNum % 2 == 0):
+        print(f"{inputNum}: is an even number")
+    elif (inputNum % 2 == 1):
+        print(f"{inputNum}: is an odd number")
+    else:
+        print("Sorry not a valid input")
+#i. 4
+isEven(4)
+#ii 9
+isEven(9)
+#iii
+isEven(19)
+#iv
+isEven(20)
