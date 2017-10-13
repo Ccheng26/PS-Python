59de8fe3279e2e52cad824698c92cb95c00bc8a2
Connie Cheng <connie.cheng52@gmail.com>
2017-10-12 20:36:37 -0400

    add ex for 10.12

--- classExercises/classex10.12.py
+++ classExercises/classex10.12.py
@@ -5,12 +5,18 @@ def to_ten():
     for i in range(1,11):
         list_to_ten.append(i)
     print(list_to_ten)
-
 to_ten()
 
+# other way to do this
+# x=[]
+# y=1
+# for i in range(0,10):
+#     list_to_ten[i]=y
+#     y=y+1
+# print(x)
 list_to_twenty=[]
 def to_twenty():
-    for i in range(1,20,2):
+    for i in range(1,22,2):
         list_to_twenty.append(i)
     print(list_to_twenty)
 
